3d2c10ec05f0f343bfd46dcf4ae50fa9f398bcf8
dima-kudinov <dima-kudinov@mail.ru>
2020-03-20 01:00:06 +0400

    Выполнено

---
+++ PizzeriaBusinessLogic/Interfaces/IStorageLogic.cs
@@ -0,0 +1,18 @@
+﻿using PizzeriaBusinessLogic.BindingModels;
+using PizzeriaBusinessLogic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzeriaBusinessLogic.Interfaces
+{
+    public interface IStorageLogic
+    {
+        List<StorageViewModel> GetList();
+        StorageViewModel GetElement(int id);
+        void AddElement(StorageBindingModel model);
+        void UpdElement(StorageBindingModel model);
+        void DelElement(int id);
+        void FillStorage(StorageIngredientBindingModel model);
+    }
+}
