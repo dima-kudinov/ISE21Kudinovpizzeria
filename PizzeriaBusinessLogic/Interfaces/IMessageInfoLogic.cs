378494bed29678127621975356242bfecee2434c
dima-kudinov <dima-kudinov@mail.ru>
2020-05-15 13:27:15 +0400

    По методичке до конфига

---
+++ PizzeriaBusinessLogic/Interfaces/IMessageInfoLogic.cs
@@ -0,0 +1,15 @@
+﻿using PizzeriaBusinessLogic.BindingModels;
+using PizzeriaBusinessLogic.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzeriaBusinessLogic.Interfaces
+{
+    public interface IMessageInfoLogic
+    {
+        List<MessageInfoViewModel> Read(MessageInfoBindingModel model);
+
+        void Create(MessageInfoBindingModel model);
+    }
+}
---
+++ PizzeriaDatabaseImplement/Implements/MessageInfoLogic.cs
@@ -0,0 +1,58 @@
+﻿using PizzeriaBusinessLogic.BindingModels;
+using PizzeriaBusinessLogic.Interfaces;
+using PizzeriaBusinessLogic.ViewModels;
+using PizzeriaDatabaseImplement.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PizzeriaDatabaseImplement.Implements
+{
+    public class MessageInfoLogic : IMessageInfoLogic
+    {
+        public void Create(MessageInfoBindingModel model)
+        {
+            using (var context = new PizzeriaDatabase())
+            {
+                MessageInfo element = context.MessageInfoes.FirstOrDefault(rec =>
+                rec.MessageId == model.MessageId);
+                if (element != null)
+                {
+                    throw new Exception("Уже есть письмо с таким идентификатором");
+                }
+
+                int? clientId = context.Clients.FirstOrDefault(rec => rec.Email ==
+                model.FromMailAddress)?.Id;
+
+                context.MessageInfoes.Add(new MessageInfo
+                {
+                    MessageId = model.MessageId,
+                    ClientId = clientId,
+                    SenderName = model.FromMailAddress,
+                    DateDelivery = model.DateDelivery,
+                    Subject = model.Subject,
+                    Body = model.Body
+                });
+                context.SaveChanges();
+            }
+        }
+
+        public List<MessageInfoViewModel> Read(MessageInfoBindingModel model)
+        {
+            using (var context = new PizzeriaDatabase())
+            {
+                return context.MessageInfoes
+                    .Where(rec => model == null || rec.ClientId == model.ClientId)
+                    .Select(rec => new MessageInfoViewModel
+                    {
+                        MessageId = rec.MessageId,
+                        SenderName = rec.SenderName,
+                        DateDelivery = rec.DateDelivery,
+                        Subject = rec.Subject,
+                        Body = rec.Body
+                    })
+                    .ToList();
+            }
+        }
+    }
+}
