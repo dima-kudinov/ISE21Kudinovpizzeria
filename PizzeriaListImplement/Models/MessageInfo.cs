3d1e861de425a07d91fc33b603086dddcc97be34
dima-kudinov <dima-kudinov@mail.ru>
2020-06-10 22:03:37 +0400

    Убраны лишние модели

--- PizzeriaListImplement/Models/MessageInfo.cs
+++ PizzeriaListImplement/Models/MessageInfo.cs
@@ -12,6 +12,5 @@ namespace PizzeriaListImplement.Models
         public DateTime DateDelivery { get; set; }
         public string Subject { get; set; }
         public string Body { get; set; }
-        public virtual Client Client { get; set; }
     }
 }
